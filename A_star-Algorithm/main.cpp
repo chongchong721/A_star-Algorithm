711d18ab138d3bcd458df8480148972f10951f0f
chongchong721 <fbzq125689@gmail.com>
2020-10-18 21:23:47 +0800

    解决8-Puzzle

--- A_star-Algorithm/main.cpp
+++ A_star-Algorithm/main.cpp
@@ -1,1 +1,104 @@
 #include <iostream>
+#include "status.h"
+
+std::vector<status*> statlist;
+std::vector<status*> closed;
+status* destination = nullptr;
+void search(status* , field* );
+void showRoute(std::vector<status*>);
+
+int main() {
+	int n;
+	std::vector<int> init;
+	std::vector<int> end;
+	std::cout << "n*n?:" << std::endl;
+	std::cin >> n;
+	std::cout << "initial status:" << std::endl;
+	for (int i = 0; i < n*n; i++) {
+		int temp;
+		std::cin >> temp;
+		init.push_back(temp);
+	}
+	std::cout << "end status:" << std::endl;
+	for (int i = 0; i < n*n; i++) {
+		int temp;
+		std::cin >> temp;
+		end.push_back(temp);
+	}
+
+	field* fstart = new field(n, init, 0);
+	field* fend = new field(n, end, 0x7FFFFFFF);
+
+	status* s = new status(fstart, nullptr, fend);
+
+	search(s, fend);
+	if (destination == nullptr) {
+		std::cout << "NOT Solvable" << std::endl;
+	}
+	else {
+		status* temproute = destination;
+		std::vector<status*> route;
+		while (temproute != nullptr) {
+			route.push_back(temproute);
+			temproute = temproute->parent;
+		}
+		showRoute(route);
+	}
+	system("pause");
+
+}
+
+void search(status* stat, field* dest) {
+	if (stat->isFinal(dest)) {
+		destination = stat;
+		return;
+	}
+
+	closed.push_back(stat);
+
+	for (int i = 0; i < 4; i++) {
+		field* temp;
+		temp=stat->s->move((MyEnum)i);
+		if (temp != nullptr) {
+			status* tempStatus = new status(temp, stat, dest);
+			bool flagPush = true;
+			for (auto iterator = closed.begin(); iterator != closed.end(); iterator++) {
+				if (tempStatus->isEqual(*iterator)) {
+					flagPush = false;
+					break;
+				}
+			}
+			if (flagPush) {
+				statlist.push_back(tempStatus);
+			}
+		}
+	}
+
+	status* temp;
+	std::vector<status*>::iterator iter;
+	int minh = 0x7FFFFFFF;
+	for (auto iterator=statlist.begin(); iterator!=statlist.end(); iterator++) {
+		if ((*iterator)->h < minh) {
+			iter = iterator;
+			minh = (*iterator)->h;
+		}
+	}
+
+	temp = (*iter);
+	statlist.erase(iter);
+	search(temp, dest);
+
+}
+void showRoute(std::vector<status*> route) {
+	std::reverse(std::begin(route), std::end(route));
+	std::cout << "----------------------------------------------------------------------------" << std::endl;
+	for (auto iterator = route.begin(); iterator != route.end(); iterator++) {
+		for (int i = 0; i < (*iterator)->s->n; i++) {
+			for (int j = 0; j < (*iterator)->s->n; j++) {
+				std::cout << (*iterator)->s->myfield[i][j] << " ";
+			}
+			std::cout << std::endl;
+		}
+		std::cout << "----------------------------------------------------------------------------" << std::endl;
+	}
+}
--- A_star-Algorithm/status.h
+++ A_star-Algorithm/status.h
@@ -1,46 +1,168 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <utility>
 
 enum MyEnum
 {
 	left,right,up,down
 };
 
-class status {
-	
-};
+
 
 class field {
+
 public:
 
 	std::vector<std::vector<int>> myfield;
 	int n;
+	std::vector<std::pair<int, int>> cost_map;
+	int depth;
 
-	field(int n,int* arr) {
-		n = this->n;
+	field(int num, std::vector<int> arr,int depth) {
+		this->n = num;
+		myfield.resize(n);
+		this->depth = depth;
 		for (int i = 0; i < n; i++) {
 			myfield[i].resize(n);
 			for (int j = 0; j < n; j++) {
-				myfield[i].push_back(arr[j + n * i]);
+				myfield[i][j]=arr[j + n * i];
+			}
+		}
+		int temp = n * n ;
+		cost_map.resize(temp);
+		for (int si = 0; si < n; si++) {
+			for (int sj = 0; sj < n; sj++) {
+				cost_map[myfield[si][sj]].first = si;
+				cost_map[myfield[si][sj]].second = sj;
+			}
+		}
+		
+	}
+
+	void re_calculate() {
+		int temp = n * n;
+		for (int i = 1; i < temp; i++) {
+			for (int si = 0; si < n; si++) {
+				for (int sj = 0; sj < n; sj++) {
+					cost_map[myfield[si][sj]].first = si;
+					cost_map[myfield[si][sj]].second = sj;
+				}
 			}
 		}
 	}
 
-	field move(MyEnum dir) {
+	field* move(MyEnum dir) {
 
-		field temp = *this;
-		int i, j;
-		switch (dir) {
-		case left:
-			for (i = 0; i < temp.n; i++) {
-				for (j = 0; j < temp.n; j++) {
-					if (temp.myfield[i][j] == 0) {
-						break;
-					}
+		field* temp = new field(*this);
+		int i;
+		int j;
+
+		bool flag = true;
+		for (i = 0; i < temp->n; i++) {
+			if (!flag) {
+				break;
+			}
+			for (j = 0; j < temp->n; j++) {
+				if (temp->myfield[i][j] == 0) {
+					flag = false;
+					break;
 				}
 			}
+		}
+		i--;
+		switch (dir) {
+		case left:
+			if (j == 0) {
+				return nullptr;
+			}
+			else {
+				std::swap(temp->myfield[i][j], temp->myfield[i][j - 1]);
+				temp->depth++;
+				temp->re_calculate();
+				return temp;
+			}
+			break;
+		case right:
+			if (j == n-1) {
+				return nullptr;
+			}
+			else {
+				std::swap(temp->myfield[i][j], temp->myfield[i][j + 1]);
+				temp->depth++;
+				temp->re_calculate();
+				return temp;
+			}
+			break;
+		case up:
+			if (i == 0) {
+				return nullptr;
+			}
+			else {
+				std::swap(temp->myfield[i][j], temp->myfield[i - 1][j]);
+				temp->depth++;
+				temp->re_calculate();
+				return temp;
+			}
+			break;
+		case down:
+			if (i == n-1) {
+				return nullptr;
+			}
+			else {
+				std::swap(temp->myfield[i][j], temp->myfield[i + 1][j]);
+				temp->depth++;
+				temp->re_calculate();
+				return temp;
+			}
+
 
 		}
 	}
 };
+
+class status {
+public:
+	field* s;
+	status* parent;
+	int cost;
+	int h;
+	
+	status(field* s,status* parent,field * dest) {
+		this->s = s;
+		this->parent = parent;
+		calculate_cost(dest);
+		h = cost + s->depth;
+	}
+
+	bool isFinal(field* dest) {
+		for (int i = 0; i < s->n; i++) {
+			for (int j = 0; j < s->n; j++) {
+				if (dest->myfield[i][j] != s->myfield[i][j]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	void calculate_cost(field* dest) {
+		int n = s->n;
+		cost = 0;
+		for (int i = 1; i <n*n; i++) {
+			cost = cost + std::abs(s->cost_map[i].first - dest->cost_map[i].first) + std::abs(s->cost_map[i].second - dest->cost_map[i].second);
+		}
+	}
+
+	bool isEqual(status* inList) {
+		for (int i = 0; i < this->s->n; i++) {
+			for (int j = 0; j < this->s->n; j++) {
+				if (this->s->myfield[i][j] != inList->s->myfield[i][j]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+};
+
